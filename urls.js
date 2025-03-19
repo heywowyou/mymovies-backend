3f86caa5b9a38802d690858c15568ed961e978bd
heywowyou <pro@julesfontaine.com>
2025-03-19 10:46:00 +0100

    Add both frontend and backend url to urls.js

--- urls.js
+++ urls.js
@@ -1,7 +1,7 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = '';
+const BACKEND_URL = "https://mymovies-backend-seven.vercel.app";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = '';
+const FRONTEND_URL = "mymovies-frontend-sandy.vercel.app";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
