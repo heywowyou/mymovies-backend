61382f25211596de93170b175785b261928c1e7b
heywowyou <pro@julesfontaine.com>
2025-03-19 10:46:26 +0100

    Edit frntend url to contain https://

--- urls.js
+++ urls.js
@@ -2,6 +2,6 @@
 const BACKEND_URL = "https://mymovies-backend-seven.vercel.app";
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = "mymovies-frontend-sandy.vercel.app";
+const FRONTEND_URL = "https://mymovies-frontend-sandy.vercel.app";
 
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
